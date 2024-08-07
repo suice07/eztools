7d88d3dd590a28b04f9d6f038bde38b72421f3cc
Auier <qi.mei@outlook.com>
2024-08-07 15:56:00 +0800

    add column keyword filter

--- eztools/__init__.py
+++ eztools/__init__.py
@@ -2,10 +2,10 @@
 Author: Auier qi.mei@outlook.com
 Date: 2024-08-07 10:15:45
 LastEditors: Auier qi.mei@outlook.com
-LastEditTime: 2024-08-07 15:28:37
+LastEditTime: 2024-08-07 15:55:47
 Copyright (c) 2024 by Auier qi.mei@outlook.com, All Rights Reserved. 
 '''
 from .utils import dictdump, loaddict
-from .dftool import rename_columns, keyword_filter
+from .dftool import rename_columns, keyword_filter, keyword_column_filter
 
-__all__ = ['dictdump','loaddict', 'rename_columns', 'keyword_filter']
+__all__ = ['dictdump','loaddict', 'rename_columns', 'keyword_filter', 'keyword_column_filter']
--- eztools/dftool.py
+++ eztools/dftool.py
@@ -2,7 +2,7 @@
 Author: Auier qi.mei@outlook.com
 Date: 2024-08-07 10:53:34
 LastEditors: Auier qi.mei@outlook.com
-LastEditTime: 2024-08-07 15:28:10
+LastEditTime: 2024-08-07 15:55:22
 Copyright (c) 2024 by Auier qi.mei@outlook.com, All Rights Reserved. 
 '''
 def rename_columns(indf, ori_column, new_column):
@@ -22,4 +22,18 @@ def keyword_filter(df, checklist):
     '''
     subdf_contains = df[df.apply(lambda row: any(keyword in str(row.values) for keyword in checklist), axis=1)]
     subdf_not_contains = df[~df.apply(lambda row: any(keyword in str(row.values) for keyword in checklist), axis=1)]
+    return subdf_contains, subdf_not_contains
+
+def contains_keyword(s, keywords):
+    return any(kw in s for kw in keywords)
+
+def keyword_column_filter(df, checklist, columnslist):
+    '''
+        given a checklist of keywords, and a columnlist, returns
+            subdf_contains: if any column in the column list contains the keyword in the checklist
+            subdf_not_contains: not any column in the column list contains any keyword in the keyword checklists
+    '''
+    mask = df[checklist].applymap(contains_keyword).any(axis = 1)
+    subdf_contains = df[mask]
+    subdf_not_contains = df[~mask]
     return subdf_contains, subdf_not_contains
